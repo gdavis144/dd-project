d0552f8ae68bc7eaf50e2d6069d6f91146f97d6f
unknown <davis.gi@northeastern.edu>
2024-04-17 20:56:44 -0400

    logging in

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -26,15 +26,15 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="email"
             >
-              Email
+              Username
             </label>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
-                type="email"
+                type="string"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="Enter your username"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
